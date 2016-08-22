40bbcfceb93b806bbb4722c4b6bd8a393db73c1a
fanfanqu <18829291202@163.com>
2016-08-22 08:56:37 +0800

    products

--- app/api/api.js
+++ app/api/api.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import userApi from './users-api'
-import loginApi from './sessions-api'
-import personApi from './personal-api'
+import userApi from './users-api';
+import loginApi from './sessions-api';
+import personApi from './personal-api';
+import productApi from './products-api';
 
 const router = express.Router();
 
 router.use('/users', userApi);
 router.use('/sessions', loginApi);
-router.use('/personal', loginApi);
+// router.use('/personal', loginApi);
+router.use('/products',productApi)
 
 export default router;
